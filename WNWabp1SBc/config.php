5d23f990a4ea7c345aeda738e45951e381e6b8ab
SolodukhaAlex <solodukha.as@gmail.com>
2020-03-14 17:46:15 +0200

    Install open cart, add custom style, add custom theme, chanch currency

---
+++ WNWabp1SBc/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://betaviz.opencart/WNWabp1SBc/');
+define('HTTP_CATALOG', 'http://betaviz.opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://betaviz.opencart/WNWabp1SBc/');
+define('HTTPS_CATALOG', 'http://betaviz.opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OSPanel/domains/betaviz.opencart/WNWabp1SBc/');
+define('DIR_SYSTEM', 'D:/OSPanel/domains/betaviz.opencart/system/');
+define('DIR_IMAGE', 'D:/OSPanel/domains/betaviz.opencart/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'D:/OSPanel/domains/betaviz.opencart/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'betaviz_admin');
+define('DB_PASSWORD', 'cgDk77alt72sZxKV');
+define('DB_DATABASE', 'betaviz_bd');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
